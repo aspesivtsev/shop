e8b5627f4e03a33d99b944bbe9faf0041b6c5fa3
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-07-16 22:12:54 +0300

    products

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/products_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   ProductsOverviewScreen({Key? key}) : super(key: key);
