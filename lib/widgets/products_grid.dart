f0e9db0ed13e3faec7405d70d16a0c561589e2f5
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-07-24 09:17:34 +0300

    Consumer2

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -17,8 +17,13 @@ class ProductsGrid extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10),
+      //this approach is usually better to use in main.dart file
       itemBuilder: (ctx, i) => ChangeNotifierProvider(
           create: (c) => products[i],
+
+          ///or you can use shorter way (.value), and it works better with lists
+          ///itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+          ///value: products[i],)...
           child: ProductItem(
               //products[i].id, products[i].title, products[i].imageUrl
               )),
