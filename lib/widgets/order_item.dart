fb11a119113834060cd373fcfea18b4209c1e6ad
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-10-24 12:09:13 +0300

    added order lists

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +23,43 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          //тут проверяется количество товаров, если их мало, то количество товаров умножаем на 20 пикселей и добавляем 100
+          //а если там очень много товаров, то тогда устонавливаем максимальную высоту 180 пикселей.
+          //а функция min() возвращает то значение, которое меньше
+          if (_expanded)
+            Container(
+              height: min(widget.order.products.length * 20 + 10, 100),
+              padding: EdgeInsets.symmetric(),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text('${prod.quantity} x \$${prod.price}')
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
